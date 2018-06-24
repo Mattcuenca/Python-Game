56c6f1ea9d38deeb3a0faa85c2c220eb5abb329d
mattcuenca <mattcuenca@gmail.com>
2018-06-23 21:36:23 -0700

    Guessing game complete

--- python-game.py
+++ python-game.py
@@ -1,24 +1,32 @@
 import random
 
-number = random.randint(1, 10)
-tries = 1
+number = random.randint(1, 100)
+game_over = False
 
-username = input("Hello, What is your username?")
+username = input("Hello, What is your username? ")
 
-print ("hello", username + ".",)
+print("hello", username + ".", )
 
 question = input("would you like to play a game? [Y/N] " )
 
 if question == "n":
     print("oh.. okay")
 if question == "y":
-    print("okay, I'm thinking of a number between 1 and 5")
-    guess = int(input("have a guess:"))
-    if guess > number:
-        print("too high")
-    if guess < number:
-        print("too low")
-    if guess == number:
-        print("You got it right! the correct number was", number + \
-        "Great Guess!")
+    print("okay, I'm thinking of a number between 1 and 100")
+    while game_over == False:
+
+        guess = int(input("have a guess: "))
+        if guess > number:
+            print("too high!")
+        if guess < number:
+            print("too low!")
+        if guess == number:
+            print("correct!")
+            game_over == True
+            break
+    
+ 
+    
+
+
 
